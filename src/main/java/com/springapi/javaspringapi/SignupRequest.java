89cd46015e307e6dc6b24107ccee6a2df1d9f543
Andriy-Kolokolov <koloandre22@gmail.com>
2023-08-06 14:22:51 +0200

    add authentication and security

---
+++ src/main/java/com/springapi/javaspringapi/SignupRequest.java
@@ -0,0 +1,11 @@
+package com.springapi.javaspringapi;
+
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+    private String username;
+    private String email;
+    private String password;
+
+}
